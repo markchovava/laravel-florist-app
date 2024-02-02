3bef1b3c2ff3c9ab6cc97357377cc6d07dddbb32
markcus <markchovava@gmail.com>
2024-02-02 11:00:54 +0200

    Removed __ref

--- __ref/laravel-ecommerce-electronics-main/resources/views/backend/category/test.blade.php
+++
@@ -1,6 +0,0 @@
-@foreach($categories as $category)
-    <h2>{{ $category->name}}</h2>
-    @foreach($category->products as $a)
-        <p>{{$a->name}}</p>
-    @endforeach
-@endforeach
--- __ref/laravel-ecommerce-electronics-main/resources/views/backend/products/test.blade.php
+++
@@ -1,6 +0,0 @@
-@foreach($products as $product)
-    <h2>{{ $product->name}}</h2>
-    @foreach($product->categories as $a)
-        <p>{{$a->name}}</p>
-    @endforeach
-@endforeach
